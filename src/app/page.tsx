ad5ac9c1d96ead309d3a9c7b7e719b7e44a70722
Aemal Sayer <a@smartgeeks.io>
2024-06-05 21:48:59 +0200

    added questions.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
+import myData from '../data/questions.json';
+
 
 export default function Home() {
+  useEffect(() => {
+    console.log("data:", myData);
+  }, []);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
