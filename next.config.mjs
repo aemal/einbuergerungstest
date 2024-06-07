61e638a04d014888b2f663f19ac8d287a1251ee6
Aemal Sayer <a@smartgeeks.io>
2024-06-07 21:58:44 +0200

    fixed pwa issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import nextPWA from 'next-pwa';
 
 const withPWA = nextPWA({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = {
