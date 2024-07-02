c30e97d6a46831f8ed53378a887fb45b06e3780c
Aemal Sayer <a@smartgeeks.io>
2024-07-02 22:26:34 +0200

    fixed production check

--- src/components/LoadUmamiScript.tsx
+++ src/components/LoadUmamiScript.tsx
@@ -1,14 +1,17 @@
+// components/LoadUmamiScript.tsx
 'use client';
 
 import { useEffect } from 'react';
 
 const LoadUmamiScript = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://cloud.umami.is/script.js';
-    script.defer = true;
-    script.dataset.websiteId = '37464e74-715f-4803-a106-582d95f4710b';
-    document.body.appendChild(script);
+    if (process.env.NODE_ENV === 'production') {
+      const script = document.createElement('script');
+      script.src = 'https://cloud.umami.is/script.js';
+      script.defer = true;
+      script.dataset.websiteId = '37464e74-715f-4803-a106-582d95f4710b';
+      document.body.appendChild(script);
+    }
   }, []);
 
   return null;
