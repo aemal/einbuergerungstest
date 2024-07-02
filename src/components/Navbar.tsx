e225c67ba27527ae5fc84d82aad7da8e38f24010
Aemal Sayer <a@smartgeeks.io>
2024-07-02 22:31:37 +0200

    fixed burger button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,13 +19,11 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div id="navbar-button" className="fixed top-2 left-2 z-40 flex justify-between items-center rounded-md bg-blue-500 p-2 w-12 h-12 transition-transform duration-300 ease-in-out">
-        <button onClick={toggleDrawer} className="text-white focus:outline-none">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-          </svg>
-        </button>
-      </div>
+      <button id="navbar-button" onClick={toggleDrawer} className="fixed top-2 left-2 z-40 flex justify-between items-center rounded-md bg-blue-500 p-2 w-12 h-12 transition-transform duration-300 ease-in-out text-white focus:outline-none">
+        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+        </svg>
+      </button>
       {isDrawerOpen && (
         <div className="fixed inset-0 z-40">
           <div className="fixed inset-0 bg-black opacity-50" onClick={closeDrawer}></div>
