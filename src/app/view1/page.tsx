2d44de33ed847fc667d27e9f32eb03861daebf17
Aemal Sayer <a@smartgeeks.io>
2024-07-03 11:28:24 +0200

    small ux fix

--- src/app/view1/page.tsx
+++ src/app/view1/page.tsx
@@ -3,11 +3,15 @@ import myData from "@/data/questions.json";
 import QuestionList from "@/components/QuestionList";
 import useScrollPosition from "@/hooks/useScrollPosition";
 import { Data } from "@/types";
-import { useEffect } from "react";
 
 export default function View1() {
   const data: Data = myData;
   useScrollPosition("scrollPositionTestPage");
-  
-  return <QuestionList data={data} randomQuestions={false} />;
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center mb-5 mt-20">310 Questions</h1>
+      <p className="text-center pl-5 pr-5">You can keep scrolling, leave the app, come back and it will resume from the same scroll position.</p>
+      <QuestionList data={data} randomQuestions={false} />
+    </>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,6 @@ const Navbar = () => {
             </div>
             <div className="space-y-4">
               <Link href="/view1" className={`block text-black p-2 rounded ${pathname === '/view1' ? 'bg-gray-200' : ''}`} onClick={closeDrawer}>Interactive</Link>
-              <Link href="/view2" className={`block text-black p-2 rounded ${pathname === '/view2' ? 'bg-gray-200' : ''}`} onClick={closeDrawer}>Interactive Random</Link>
               <Link href="/view3" className={`block text-black p-2 rounded ${pathname === '/view3' ? 'bg-gray-200' : ''}`} onClick={closeDrawer}>Read Only</Link>
               <Link href="/words" className={`block text-black p-2 rounded ${pathname === '/words' ? 'bg-gray-200' : ''}`} onClick={closeDrawer}>Flashcards</Link>
               <Link href="/test" className={`block text-black p-2 rounded ${pathname === '/test' ? 'bg-gray-200' : ''}`} onClick={closeDrawer}>Practice Test</Link>
