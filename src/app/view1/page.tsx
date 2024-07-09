683218c644d3f5bac3622acfebe2c9d99491030e
Aemal Sayer <a@smartgeeks.io>
2024-07-09 10:44:55 +0200

    added questions

--- src/app/view1/page.tsx
+++ src/app/view1/page.tsx
@@ -9,7 +9,7 @@ export default function View1() {
   useScrollPosition("scrollPositionTestPage");
   return (
     <>
-      <h1 className="text-3xl font-bold text-center mb-5 mt-20">310 Questions</h1>
+      <h1 className="text-3xl font-bold text-center mb-5 mt-20">350 Questions</h1>
       <p className="text-center pl-5 pr-5">You can keep scrolling, leave the app, come back and it will resume from the same scroll position.</p>
       <QuestionList data={data} randomQuestions={false} />
     </>
